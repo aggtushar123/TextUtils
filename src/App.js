c258c668cc6dbc2d13666ffccae67b0aa75fa6bd
Tushar Aggarwal <aggtushar123@gmail.com>
2023-05-28 21:31:33 +0530

    React Router Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
 
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) =>{
+      setAlert({
+        message : message,
+        type : type
+      })
+      setTimeout(() =>{
+        setAlert(null)
+      },1500);
+  }
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor= '#042743'
+      showAlert("Dark mode enabled", "success")
+    }
+   
+    else{
+      setMode('light')
+      document.body.style.backgroundColor= 'white'
+      showAlert("Light mode enabled", "success")
+    }
+  }
   return (
     <>
-    <Navbar title ="TextUtils"/>
-    <div className="container my- 3" >
+    <Router>
+    <Navbar title ="TextUtils" mode={mode} toggleMode= {toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3" >
+
+    <Routes>
+      <Route exact path='/about' element={ <About/>}/>
+     
+      <Route exact path='/' element={<TextForm heading= "Enter the text to analyze" mode = {mode}/>} />
+      
+    </Routes>
     
-      <TextForm heading= "Enter the text to analyze"/>
-    </div>
+      
+     
+      
+    </div>  
+    </Router>
+    
   </>
     );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,23 +7,52 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLowClick = () =>{
+        // console.log('Uppercase was clicked' + text)
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnChange = (event) =>{
         console.log('Onchange was clicked')
         setText(event.target.value)
     }
 
+    const handleClearText = () =>{
+        let newText = ' '
+        setText(newText)
+    }
+
+    const handleCopyText = () =>{
+        let text = document.getElementById('myBox')
+        text.select();
+        navigator.clipboard.writeText(text.value)
+    }
   
     const [text, setText] = useState('')
 
     return (
-    <div>
+        <>
+        <div className='container' style= {{color: props.mode==='dark'?'white':'#042743'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
         
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            
-            <textarea className="form-control" value = {text} onChange= {handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-            </div>
-            <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
-    </div>
+        <textarea className="form-control" value = {text} onChange= {handleOnChange} style={{backgroundColor: props.mode==='light'?'light':'grey', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick} >Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyText} >Copy Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleClearText} >Clear Text</button>
+        
+        </div>
+        
+        <div className="container my-3" style = {{color: props.mode==='dark'?'white':'#042743'}}>
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} Words and {text.length} characters</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Enter your text to preview"}</p>
+
+        </div>
+        </>
+    
   )
 }
