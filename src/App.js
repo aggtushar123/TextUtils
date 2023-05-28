c9e15ebb0025cbbca3d8043cab1d967363714ca2
Tushar Aggarwal <aggtushar123@gmail.com>
2023-05-28 21:56:03 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   return (
     <>
     <Router>
-    <Navbar title ="TextUtils" mode={mode} toggleMode= {toggleMode}/>
+    <Navbar title ="TextUtils" about = "About" mode={mode} toggleMode= {toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3" >
 
